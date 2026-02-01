52fc9cbb09534a2b15a657dcab8e9871b7919a6b
fdoganis <fivos.doganis@gmail.com>
2026-02-01 19:05:00 +0100

    feat: create main.ts, main.js is now the output

--- main.ts
+++ main.ts
@@ -1,13 +1,28 @@
 "use strict";
+
 // ⚠️ DO NOT EDIT main.js DIRECTLY ⚠️
 // This file is generated from the TypeScript source main.ts
 // Any changes made here will be overwritten.
+
 // Import only what you need, to help your bundler optimize final code size using tree shaking
 // see https://developer.mozilla.org/en-US/docs/Glossary/Tree_shaking)
-import { PerspectiveCamera, Scene, WebGLRenderer, BoxGeometry, Mesh, MeshNormalMaterial, AmbientLight, Clock } from 'three';
+
+import {
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  BoxGeometry,
+  Mesh,
+  MeshNormalMaterial,
+  AmbientLight,
+  Clock
+} from 'three';
+
 // If you prefer to import the whole library, with the THREE prefix, use the following line instead:
 // import * as THREE from 'three'
+
 // NOTE: three/addons alias is supported by Rollup: you can use it interchangeably with three/examples/jsm/  
+
 // Importing Ammo can be tricky.
 // Vite supports webassembly: https://vitejs.dev/guide/features.html#webassembly
 // so in theory this should work:
@@ -22,59 +37,93 @@ import { PerspectiveCamera, Scene, WebGLRenderer, BoxGeometry, Mesh, MeshNormalM
 // See vite.config.js
 // 
 // Consider using alternatives like Oimo or cannon-es
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {
+  OrbitControls
+} from 'three/addons/controls/OrbitControls.js';
+
+import {
+  GLTF,
+  GLTFLoader
+} from 'three/addons/loaders/GLTFLoader.js';
+
 // Example of hard link to official repo for data, if needed
 // const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/three.js/r173/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
+
+
 // INSERT CODE HERE
-var scene = new Scene();
-var aspect = window.innerWidth / window.innerHeight;
-var camera = new PerspectiveCamera(75, aspect, 0.1, 1000);
-var light = new AmbientLight(0xffffff, 1.0); // soft white light
+
+const scene = new Scene();
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new PerspectiveCamera(75, aspect, 0.1, 1000);
+
+const light = new AmbientLight(0xffffff, 1.0); // soft white light
 scene.add(light);
-var renderer = new WebGLRenderer();
+
+const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var controls = new OrbitControls(camera, renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.listenToKeyEvents(window); // optional
-var geometry = new BoxGeometry(1, 1, 1);
-var material = new MeshNormalMaterial();
-var cube = new Mesh(geometry, material);
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshNormalMaterial();
+const cube = new Mesh(geometry, material);
+
 scene.add(cube);
+
 function loadData() {
-    new GLTFLoader()
-        .setPath('assets/models/')
-        .load('test.glb', gltfReader);
+  new GLTFLoader()
+    .setPath('assets/models/')
+    .load('test.glb', gltfReader);
 }
-function gltfReader(gltf) {
-    var testModel = null;
-    testModel = gltf.scene;
-    if (testModel != null) {
-        console.log("Model loaded:  " + testModel);
-        scene.add(gltf.scene);
-    }
-    else {
-        console.log("Load FAILED.  ");
-    }
+
+function gltfReader(gltf : GLTF) {
+  let testModel = null;
+
+  testModel = gltf.scene;
+
+  if (testModel != null) {
+    console.log("Model loaded:  " + testModel);
+    scene.add(gltf.scene);
+  } else {
+    console.log("Load FAILED.  ");
+  }
 }
+
 loadData();
+
+
 camera.position.z = 3;
-var clock = new Clock();
+
+
+const clock = new Clock();
+
 // Main loop
-var animation = function () {
-    renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
-    var delta = clock.getDelta();
-    var elapsed = clock.getElapsedTime();
-    // can be used in shaders: uniforms.u_time.value = elapsed;
-    cube.rotation.x = elapsed / 2;
-    cube.rotation.y = elapsed / 1;
-    renderer.render(scene, camera);
+const animation = () => {
+
+  renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
+
+  const delta = clock.getDelta();
+  const elapsed = clock.getElapsedTime();
+
+  // can be used in shaders: uniforms.u_time.value = elapsed;
+
+  cube.rotation.x = elapsed / 2;
+  cube.rotation.y = elapsed / 1;
+
+  renderer.render(scene, camera);
 };
+
 animation();
+
 window.addEventListener('resize', onWindowResize, false);
+
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
 }
-//# sourceMappingURL=main.js.map
